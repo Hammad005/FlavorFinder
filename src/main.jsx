407840d679f4d9de2ea1cf71cde2fc9e7f1d7e5e
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-01-21 19:29:19 +0500

    more fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "@fontsource/merriweather"; // Merriweather font
+import "@fontsource/dancing-script"; // Dancing Script font
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
